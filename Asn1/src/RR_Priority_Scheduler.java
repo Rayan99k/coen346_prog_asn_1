4d9d3f1b4874a4d229228af48f96b7c147f6404a
Alex <60273062+zwaky@users.noreply.github.com>
2023-10-29 20:52:33 -0400

    Small Formating Change

--- Asn1/src/RR_Priority_Scheduler.java
+++ Asn1/src/RR_Priority_Scheduler.java
@@ -48,81 +48,69 @@ public class RR_Priority_Scheduler {
         System.out.println("    Round Robin with Priority Scheduling");
         System.out.println(dashedLine);
 
-            //Run the scheduler for each algorithm
-            for (int i = readyQueues.length -1 ; i >= 0 ; i--) { //Note reverse order because smaller is higher priority
-                Queue<Process> readyQueue = readyQueues[i];
-                Queue<Process> waitingQueue = waitingQueues[i];
+        //Run the scheduler for each algorithm
+        for (int i = readyQueues.length -1 ; i >= 0 ; i--) { //Note reverse order because smaller is higher priority
+            Queue<Process> readyQueue = readyQueues[i];
+            Queue<Process> waitingQueue = waitingQueues[i];
+            
+            System.out.println("\n              Priority:" + (i+1));
+            while (!readyQueue.isEmpty()) {                    
+                System.out.print("\n  Ready Queue: ");
+                printQueue(readyQueue);
+                System.out.print("\n  Waiting Queue: ");
+                printQueue(waitingQueue);
+                //poll(): Removes and returns the element at the front of the readyQueue.
+                Process currentProcess = readyQueue.poll();
+                currentProcess.setState("Running");
                 
-                System.out.println("\n              Priority:" + (i+1));
-
-                while (!readyQueue.isEmpty()) {                    
-
-                    System.out.print("\n  Ready Queue: ");
-                    printQueue(readyQueue);
-
-                    System.out.print("\n  Waiting Queue: ");
-                    printQueue(waitingQueue);
-
-                    //poll(): Removes and returns the element at the front of the readyQueue.
-                    Process currentProcess = readyQueue.poll();
-                    currentProcess.setState("Running");
-                    
-                    //Has it already gone through the scheduler? If not, set the first contact time
-                    if(!currentProcess.isFirstResponse()){
-                    currentProcess.setFirstResponse(true);
-                    currentProcess.setFirstResponseTime(Time.get()-currentProcess.getArrivalTime());
-                    }
-
-                    //Update waitingTime: waitingTime + (currentTime - LastServedTime)
-                    currentProcess.setWaitingTime(currentProcess.getWaitingTime()+
-                                                (Time.get()-currentProcess.getLastServed())
-                                                );
-
-                    int remainingTime = currentProcess.getBurstTime();
-
-                    //Adjust the current time and remaining time.        
-                    //Put the process back into queue if there is burstTime remaining            
-                    if (remainingTime > quantum){
-                        currentProcess.setBurstTime(remainingTime-quantum);
-                        Time.inc(quantum);                
-                        currentProcess.setState("Ready");
-                        readyQueue.add(currentProcess);
-                    }
-
-                    //Process is done 
-                    //Put into the Done queue and get its parent if it was the last child
-                    else{            
-                        currentProcess.setBurstTime(0);            
-                        Time.inc(remainingTime);
-                        currentProcess.setState("Done");
-                        currentProcess.setCompletionTime(Time.get()-currentProcess.getArrivalTime());
-                        doneQueue.add(currentProcess);
-
-                        //Go get the parent 
-                        Process parent = currentProcess.getParent();
-
-                        if (parent != null) {
-                            parent.decChildren(); // Decrement the children count
-                            
-                            //If the current process was the last child, put the parent in the ready queue
-                            if (parent.getChildren() == 0) {
-                                waitingQueue.remove(parent); // Remove the parent from waitingQueue
-                                readyQueue.add(parent); // Add the parent to readyQueue
-                                parent.setState("Ready");
-                            }
-                        }    
-                    
-                    }
-
-                    currentProcess.setLastServed(Time.get());
-
-                    System.out.println("\nCurrent Time: " + Time.get() + 
-                    "   Process: " + currentProcess.getName() + 
-                    "   Remaining CPU: " + currentProcess.getBurstTime()  +
-                    "\n"
-                    );
+                //Has it already gone through the scheduler? If not, set the first contact time
+                if(!currentProcess.isFirstResponse()){
+                currentProcess.setFirstResponse(true);
+                currentProcess.setFirstResponseTime(Time.get()-currentProcess.getArrivalTime());
                 }
-            } 
+                //Update waitingTime: waitingTime + (currentTime - LastServedTime)
+                currentProcess.setWaitingTime(currentProcess.getWaitingTime()+
+                                            (Time.get()-currentProcess.getLastServed())
+                                            );
+                int remainingTime = currentProcess.getBurstTime();
+                //Adjust the current time and remaining time.        
+                //Put the process back into queue if there is burstTime remaining            
+                if (remainingTime > quantum){
+                    currentProcess.setBurstTime(remainingTime-quantum);
+                    Time.inc(quantum);                
+                    currentProcess.setState("Ready");
+                    readyQueue.add(currentProcess);
+                }
+                //Process is done 
+                //Put into the Done queue and get its parent if it was the last child
+                else{            
+                    currentProcess.setBurstTime(0);            
+                    Time.inc(remainingTime);
+                    currentProcess.setState("Done");
+                    currentProcess.setCompletionTime(Time.get()-currentProcess.getArrivalTime());
+                    doneQueue.add(currentProcess);
+                    //Go get the parent 
+                    Process parent = currentProcess.getParent();
+                    if (parent != null) {
+                        parent.decChildren(); // Decrement the children count
+                        
+                        //If the current process was the last child, put the parent in the ready queue
+                        if (parent.getChildren() == 0) {
+                            waitingQueue.remove(parent); // Remove the parent from waitingQueue
+                            readyQueue.add(parent); // Add the parent to readyQueue
+                            parent.setState("Ready");
+                        }
+                    }    
+                
+                }
+                currentProcess.setLastServed(Time.get());
+                System.out.println("\nCurrent Time: " + Time.get() + 
+                "   Process: " + currentProcess.getName() + 
+                "   Remaining CPU: " + currentProcess.getBurstTime()  +
+                "\n"
+                );
+            }
+        } 
         
     
         //At the end of the run, calculate performance
